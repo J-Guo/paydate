2b2e452b4094def625fd5dbb5fe80646d3a8a670
Junjie <jguo1495@sydney.edu.au>
2016-02-03 19:55:41 +1100

    Sign in page has been updated

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -3,22 +3,27 @@
 
 @section('content')
 
-    <form class="form-horizontal" action="{{URL('signin')}}"  method="POST" id="signin-form">
+    <div class="wrapper-page">
 
-        <div class="login-field ">
-            <input class="form-control" type="text"  placeholder="Mobile Number" name="email"  required>
-            <i class="md md-phone form-control-feedback l-h-34"></i>
+        <div class="text-center">
+            <a href="#" class="logo logo-lg"><i class="md md-equalizer"></i> </a>
         </div>
-        <div class="login-field ">
-            <input class="form-control" type="password"   name="password" required>
-            <i class="md md-phone form-control-feedback l-h-34"></i>
-        </div>
-        {{csrf_field()}}
-        <div class="login-field ">
-            <button class="btn btn-block w-lg btn-purple waves-effect waves-light" id="userSubmit">Sign In
-            </button>
-        </div>
-    </form>
+        <form class="form-horizontal" action="{{URL('signin')}}"  method="POST" id="signin-form">
+
+            <div class="login-field ">
+                <input class="form-control" type="text"  placeholder="Email Address" name="email"  required>
+            </div>
+            <div class="login-field ">
+                <input class="form-control" type="password" placeholder="Password"  name="password" required>
+            </div>
+            {{csrf_field()}}
+            <div class="login-field ">
+                <button class="btn btn-block w-lg btn-purple waves-effect waves-light" id="userSubmit">Sign In
+                </button>
+            </div>
+        </form>
+
+    </div>
 
 @stop
 
