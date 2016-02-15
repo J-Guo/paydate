8e7fbea23f007b264addb1318768f48a85ae9032
Junjie <jguo1495@sydney.edu.au>
2016-02-15 19:34:36 +1100

    Google Map info window has been added

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,6 @@
 /**
  * Created by Eric.G on 13/01/2016.
+ * Build functions for Google Map
  */
 
    // confirm('Paydate wants to know your location');
@@ -27,10 +28,32 @@ var app = new Vue({
             geocoder.geocode({address:this.address},function(results,status){
                 console.log(status,results);
 
+                //image address for info window
+                var imgURL = 'images/1.jpg';
+                var imgURL2 = 'images/2.jpg';
+                var imgURL3 = 'images/3.jpg';
+                var imgURL4 = 'images/4.jpg';
+
+                //google map infoWindow content
+                var contentPublic ='Luce Allen'+
+                    '<br><img src="'+imgURL+'" style="height:50px;width:50px;">' +
+                    '<img src="'+imgURL2+'" style="height:50px;width:50px;">'+
+                    '<img src="'+imgURL3+'" style="height:50px;width:50px;">'+
+                    '<img src="'+imgURL4+'" style="height:50px;width:50px;">'+'<br>'+
+                    '<a href="aprofile">'+
+                    'Go To Her Profile!</a>';
+
+                var contentPrivate ="Oops.Her profile is private";
+
+                //google map infoWindow
+                var infowindow = new google.maps.InfoWindow({
+                    maxWidth: 200
+                });
+
                 //if the return results are avaiable, show the first one on the map
                 if(status === google.maps.GeocoderStatus.OK){
 
-                    vm.map.setZoom(15);
+                    vm.map.setZoom(13);
                     vm.map.setCenter(results[0].geometry.location);
 
                     //initialize random markers neary research
@@ -39,11 +62,24 @@ var app = new Vue({
                     for (i = 0; i < 10; i++) {
                         marker = new google.maps.Marker({
                             icon: iconBase + 'female.png',
-                            position: new google.maps.LatLng(results[0].geometry.location.lat()*getRandomArbitrary(0.9998,1),
-                                results[0].geometry.location.lng()*getRandomArbitrary(0.9998,1)),
+                            position: new google.maps.LatLng(results[0].geometry.location.lat()*getRandomArbitrary(0.9998,1.0002),
+                                results[0].geometry.location.lng()*getRandomArbitrary(0.9998,1.0002)),
                             map:vm.map
 
-                        });}
+                        });
+
+                        //build popup information for each marker
+                        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                            return function() {
+                                if(getRandomOneTwo() == 1)
+                                    infowindow.setContent(contentPublic);
+                                else
+                                    infowindow.setContent(contentPrivate);
+                                infowindow.open(vm.map, marker);
+                            }
+                        })(marker, i));
+
+                    }
 
 
                     //build a maker for the map
@@ -66,5 +102,14 @@ var app = new Vue({
  */
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random 0 or 1
+ */
+function getRandomOneTwo(){
+
+    return Math.floor(Math.random()*10)%2;
+
 }
 
